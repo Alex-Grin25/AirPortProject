8d01d7153fba8682edc7fd38b46e4cd066ff137f
Alex-Grin25 <lona.ai@yahoo.com>
2021-06-22 15:03:58 +0300

    Add navigation button

--- AirPortG5/MainViewController.h
+++ AirPortG5/MainViewController.h
@@ -2,13 +2,13 @@
 //  MainViewController.h
 //  AirPortG5
 //
-//  Created by Rodion Molchanov on 16.06.2021.
+//  Created by Alexander Grigorev on 22.06.2021.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface MainViewController : UIViewController
-
+@property (nonatomic, strong) NSNumber *num;
 
 @end
 
--- AirPortG5/MainViewController.m
+++ AirPortG5/MainViewController.m
@@ -2,20 +2,34 @@
 //  MainViewController.m
 //  AirPortG5
 //
-//  Created by Rodion Molchanov on 16.06.2021.
+//  Created by Alexander Grigorev on 22.06.2021.
 //
 
 #import "MainViewController.h"
 #import "DataManager.h"
 
 @interface MainViewController ()
-
+@property (strong, nonatomic) UIButton *button;
 @end
 
 @implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if (self.num == nil)
+        self.num = @1;
+    self.title = [NSString stringWithFormat:@"Controller %@", self.num];
+    
+    self.button = [UIButton new];
+    CGSize size = CGSizeMake(self.view.frame.size.width * 0.8, self.view.frame.size.height * 0.1);
+    CGPoint origin = CGPointMake((self.view.frame.size.width - size.width) / 2, (self.view.frame.size.height - size.width) / 2);
+    self.button.frame = CGRectMake(origin.x, origin.y, size.width, size.height);
+    [self.button setTitle:@"Navigate" forState:UIControlStateNormal];
+    [self.button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    self.button.hidden = YES;
+    [self.view addSubview:self.button];
+    [self.button addTarget:self action:@selector(navigate) forControlEvents:UIControlEventTouchUpInside];
+    
     
     [[DataManager sharedInstance] loadData];
     self.view.backgroundColor = UIColor.cyanColor;
@@ -23,13 +37,16 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loadDidComplete) name:kDataManagerLoadDataDidComplete object:nil];
 }
 
-
 - (void) loadDidComplete {
     self.view.backgroundColor = UIColor.greenColor;
+    self.button.hidden = NO;
 }
 
-
-
+- (void) navigate {
+    MainViewController *vc = [MainViewController new];
+    vc.num = @(self.num.intValue + 1);
+    [self.navigationController pushViewController:vc animated:YES];
+}
 
 - (void) dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
